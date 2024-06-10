2da9a7fa836589375c4e0f0f7ed187102bd55ded
Nam Son Vu <nam_son.vu@smail.th-koeln.de>
2024-06-10 02:25:30 +0200

    SettingsScreen begonnen

--- app/src/main/java/com/example/gysd/Screens/SettingsScreens.kt
+++ app/src/main/java/com/example/gysd/Screens/SettingsScreens.kt
@@ -1,24 +1,274 @@
 package com.example.gysd.Screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.gysd.ui.theme.backGroundgrey
+import com.example.gysd.ui.theme.black
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
 @Composable
 fun SettingsScreen() {
-    Box(modifier = Modifier
-        .fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier.drawBehind {
+                    val strokeWidth = 100 * density
+                    val y = size.height - strokeWidth / 2
+
+                    drawLine(
+                        Color.LightGray,
+                        Offset(0f, y),
+                        Offset(size.width, y),
+                        strokeWidth
+                    )
+                },
+
+                title = {
+                    Text("GYSD!")
+                },
+
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = backGroundgrey,
+                    titleContentColor = black,
+                ),
+            )
+        }
+
     ){
-        Text(
-            text = "Settings Screen",
-            fontFamily = FontFamily.Serif,
-            fontSize = 22.sp
-        )
+        //Hauptstruktur: Anordung aller Elemente und Elementgruppen in einer Spalte (Column)
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(backGroundgrey)
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+
+            ){
+
+
+            //Spacer(modifier = Modifier.size(10.dp))
+            NotificationsGroup()
+
+
+
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NotificationsGroup(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ){
+        // Notification-Überschrift
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            Text(
+                text = "NOTIFICATIONS",
+                style = MaterialTheme.typography.titleMedium,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+        }
+
+        // Lock screen alert - Row
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Lock Screen Alert",
+                fontSize = 20.sp,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            MySwitch()
+        }
+
+        // Push Notifications - Row
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Push Notifications",
+                fontSize = 20.sp,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            MySwitch()
+        }
+
+        // Sounds - Row
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Sounds",
+                fontSize = 20.sp,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            MySwitch()
+        }
+
+        // Vibration Notifications - Row
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Vibration Notifications",
+                fontSize = 20.sp,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            MySwitch()
+        }
+
+        Spacer(modifier = Modifier.size(25.dp))
+
+        // About_Us - Überschrift
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            Text(
+                text = "ABOUT US",
+                style = MaterialTheme.typography.titleMedium,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+        }
+
+        // Our Website - Row
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Our Website",
+                fontSize = 20.sp,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            /*  - IconButton welcher "Expandable Text" aufklappt
+                    - https://stackoverflow.com/questions/68894585/expandable-text-in-jetpack-compose
+                    - https://medium.com/@munbonecci/how-to-implement-expandable-text-in-jetpack-compose-ca9ba35b645c
+                    - https://arkadiuszchmura.com/posts/how-to-make-expandable-text-with-a-button-in-jetpack-compose/
+                - oder als "Expandable Card" implementieren
+
+             */
+        }
+
+        // Privacy Policy - Row
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Privacy Policy",
+                fontSize = 20.sp,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+
+        }
+
+        // Terms Of Service - Row
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Terms Of Service",
+                fontSize = 20.sp,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+
+        }
+
+        // Support Us - Row
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Support Us",
+                fontSize = 20.sp,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+
+        }
+    }
+}
+
+
+@Composable
+fun AboutUsGroup(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ){
+
+    }
+}
+
+@Composable
+private fun MySwitch(){
+    var switch by remember {
+        mutableStateOf(ToggleableInfo(
+            isChecked = false,
+            text = ""
+        ))
+    }
+
+    Switch(
+        checked = switch.isChecked,
+        onCheckedChange = {isChecked ->
+            switch = switch.copy(isChecked = isChecked)
+        }
+    )
 }
